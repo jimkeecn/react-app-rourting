e93e6a3572d269f30fcf5a5d96e820e3d9254e06
jimkeecn <jimkeecn@gmail.com>
2019-06-16 23:08:05 +1000

    route - nested routing

---
+++ src/components/admin/sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const SideBar = () => {
+  return (
+    <React.Fragment>
+      <ul>
+        <li>
+          <Link to="/admin/posts">Posts</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/admin/users">Users</Link>
+        </li>
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default SideBar;
